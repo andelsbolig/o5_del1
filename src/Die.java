b6ae7f6f9517f1a6a96839fecb2d219ac9f8d9de
Filip Merlung <filip.merlung@gmail.com>
2022-09-27 22:32:53 +0200

    added main class and worked on die class

--- src/Die.java
+++ src/Die.java
@@ -1,5 +1,19 @@
 public class Die {
-    public static void main(String[] args) {
-        System.out.print("Test");
+    private double value;
+
+    public double getValue() { return this.value; }
+    public void setValue(double newValue) { this.value = newValue;}
+
+    public Die () {
+    }
+
+    public void roll()
+    {
+        this.value = getNewValue();
+    }
+
+    private double getNewValue()
+    {
+        return Math.random() * 6;
     }
 }
