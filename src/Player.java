ea24a2a9a34ce87b72f35c6b76e2ba24b1c0b830
SkumJustEatMe <s214662@dtu.dk>
2022-09-28 17:39:07 +0200

    Udviklet mere på player class - Tilføjet en Die funktion

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,14 @@
 public class Player {
 
+    private String name;
+    private int sum;
+
     public Player() // Vores construktor
     {
+    This.name;
+    }
 
+    public GetSum(Die die1, Die die2)
+    {
     }
 }
